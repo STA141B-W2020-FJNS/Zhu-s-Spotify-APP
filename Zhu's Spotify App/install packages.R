bc58dc09b9cf4534edb1196eb2f9e767eb54c3e2
JZNeilZ <43965652+JZNeilZ@users.noreply.github.com>
2020-03-18 04:18:17 -0700

    Add files via upload

---
+++ Zhu's Spotify App/install packages.R
@@ -0,0 +1,11 @@
+
+devtools::install_git("charlie86/spotifyr")
+install.packages("jsonlite")
+install.packages("httr")
+install.packages("tidyverse")
+install.packages("lubridate")
+install.packages("shiny")
+install.packages("shinydashboard")
+
+
+########################## ????????? ########################
